42cd40c53354f92b5e13c49e19d8dfe321379986
bbb324 <johnigzie@163.com>
2022-08-10 23:30:43 +0800

    提交通过

--- q1.js
+++ q1.js
@@ -1,39 +1,28 @@
-/**
- * @param {number[]} asteroids
- * @return {number[]}
- */
- var asteroidCollision = function(asteroids) {
-   let list = [];
-    for(let i = 0; i<asteroids.length; i++) {
-        if(list.length === 0 || asteroids[i] > 0) {
-            list.push(asteroids[i])
-        } else {
-            while(true) {
-                let last = list[list.length - 1];
-                if((last + asteroids[i]) === 0) {
-                    list.pop();
-                    break;
-                }
-                if(last * asteroids[i] > 0) {
-                    list.push(asteroids[i]);
-                    break;
-                }
-                if(last + asteroids[i] > 0) {
-                    break;
-                }
-                if(last + asteroids[i] < 0) {
-                    list.pop();
-                    if(list.length === 0) {
-                        list.push(asteroids[i]);
-                        break;
-                    }
-                }
-       
-            }
-        } 
+var numSplits = function(s) {
+    let index = 1;
+    let sum = 0;
+    let set1 = new Set();
+    let set2 = new Set();
+    while(index < s.length) {
+        let str1 = s.slice(0, index);
+        let str2 = s.slice(index, s.length + 1);
+        if(isGood(str1, str2)) {
+            sum++;
+        }
+        index++;
     }
-    return list;
+    return sum;
 };
+function isGood(str1, str2) {
+    let set1 = new Set();
+    let set2 = new Set();
+    for(const s of str1) {
+        set1.add(s);
+    }
+    for(const s of str2) {
+        set2.add(s);
+    }
+    return set1.size === set2.size;
+}
 
-//console.log(asteroidCollision([10, 2, -5]))
-console.log(asteroidCollision([-2, -2, 1, -2]))
+console.log(numSplits("acbadbaada"))
--- q2.js
+++ q2.js
@@ -1,43 +1,20 @@
-/**
- * @param {number[]} asteroids
- * @return {number[]}
- */
- var asteroidCollision = function(asteroids) {
-    let list = [];
-    for(let i = 0; i<asteroids.length; i++) {
-        if(list.length === 0 || asteroids[i] > 0) {
-            list.push(asteroids[i])
-        } else {
-            while(true) {
-                let last = list[list.length - 1];
- 
-                // 如果相同则相互抵消
-                if(last + asteroids[i] === 0) {
-                    list.pop();
-                    break;
-                }
- 
-                // 如果出现 [1] -2 则 list pop()
-                if(last + asteroids[i] < 0) {
-                    list.pop();
-                    if(list.length === 0) {
-                        list.push(asteroids[i])
-                        break;
-                    }
-                    
-                }
-                // 如果出现 [2] -1 则直接break
-                if(last + asteroids[i] > 0) {
-                    break;
-                }
-                
-                // 如果出现相同方向[-2] -2 则直接 push 
-                if(last * asteroids[i] > 0) {
-                    list.push(asteroids[i]);
+var getFolderNames = function(names) {
+    let res = [];
+    let set = new Set();
+    for(let name of names) {
+        if(set.has(name)) {
+            for(let i = 1; ;i++) {
+                let str = `${name}(${i})`;
+                if(!set.has(str)) {
+                    set.add(str)
                     break;
                 }
             }
+        } else {
+            set.add(name)
         }
     }
-    return list;
- };
+    return [...set];
+};
+
+console.log(getFolderNames(["wano","wano","wano","wano"]))
