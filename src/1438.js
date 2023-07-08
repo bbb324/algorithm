c609465d7ac530f5a6dc974fd93a64e560da3ce3
bbb324 <johnigzie@163.com>
2023-07-08 16:48:48 +0800

    更新题解

--- src/1438.js
+++ src/1438.js
@@ -1,10 +1,20 @@
+/**
+ * 类似2762题解
+ * 需要及时更新 max, min 的大小
+ * 在做 r-- 去更新 max, min   之前！！！！
+ * 需要先设定 
+ * max = nums[l]; min = nums[l]
+ * 
+ * https://leetcode.cn/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/solutions/2335808/bbb324-shuang-zhi-zhen-by-qqqq-18-l464/
+ */
+
 var longestSubarray = function(nums, limit) {
     let ans = 0;
     let max = nums[0];
     let min = nums[0];
     let r = -1;
     for(let l = 0; l < nums.length; l++) {
-        if(max - nums[l] <= limit || nums[l] - min <= limit) {
+        if(max - nums[l] <= limit && nums[l] - min <= limit) {
             ans = Math.max(ans, l - r);
             max = Math.max(max, nums[l]);
             min = Math.min(min, nums[l]);
